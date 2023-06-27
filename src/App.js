dcd89693419e377b0389dd79ec2329688cfb04dc
Nika <nikaonime@gmail.com>
2023-06-27 13:01:16 +0300

    end logic and css styles

--- src/App.js
+++ src/App.js
@@ -6,20 +6,64 @@ import { useState } from 'react';
 function App() {
 
   const [cards, setCards] = useState(shuffle([...Images, ...Images]))
+  const [clicks, setClicks] = useState(0)
+  const [won, setWon] = useState(false)
+  const [activeCards, setActiveCards] = useState([])
+  const [foundPairs, setFoundPairs] = useState([])
+
+  function flipCard(index) {
+    if(won){
+      setCards(shuffle([...Images, ...Images]))
+      setFoundPairs([])
+      setWon(false)
+      setClicks(0)
+    }
+    if (activeCards.length === 0) {
+      setActiveCards([index])
+    }
+    if (activeCards.length === 1) {
+
+      const firstIndex = activeCards[0]
+      const secondIndex = index
+      if (cards[firstIndex] === cards[secondIndex]) {
+        if (foundPairs.length + 2 === cards.length) {
+          setWon(true)
+        }
+        setFoundPairs([...foundPairs, firstIndex, secondIndex])
+      }
+      setActiveCards([...activeCards, index])
+    }
+    if (activeCards.length === 2) {
+      setActiveCards([index])
+    }
+    setClicks(clicks + 1)
+  }
 
   return (
-    <div className='board'>
-      {cards.map((card, index) => (
-        <div className='card-outer'>
-          <div className='card'>
-            <div className='front'>
-              <img src={card} />
+    <div>
+      <div className='board'>
+        {cards.map((card, index) => {
+          const flippedToFront = (activeCards.indexOf(index) !== -1) || foundPairs.indexOf(index) !== -1;
+          return (
+            <div className={'card-outer ' + (flippedToFront ? 'flipped' : '')} onClick={() => flipCard(index)}>
+              <div className='card'>
+                <div className='front'>
+                  <img src={card} />
+                </div>
+                <div className='back'></div>
+              </div>
             </div>
-            <div className='back'></div>
-          </div>
-        </div>
-      ))}
+          )
+        })}
+      </div>
+      <div className='stats'>
+        {won && (
+          <>You won the game! Congratulations! <br /></>
+        )}
+        Clicks: {clicks} &nbsp; &nbsp; &nbsp; Found pairs: {foundPairs.length / 2}
+      </div>
     </div>
+
 
   );
 }
